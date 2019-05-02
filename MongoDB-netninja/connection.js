fe5e5ba6f7ed1ea2c7a2bd3d7ba773d558b638aa
channu <channucs0009@gmail.com>
2019-05-02 11:39:26 +0530

    adafafa

--- MongoDB-netninja/connection.js
+++ MongoDB-netninja/connection.js
@@ -2,3 +2,6 @@ const mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost/TestDB')
 .then(()=>console.log("connected to Mongdb"))
 .catch(err=>console.error("Ooops... connection error",err));
+
+
+//mmm
