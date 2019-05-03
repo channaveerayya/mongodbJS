adeb6d1e8d79f45ce9f52bea3a1fc32b64414af2
channu <43161552+channaveerayya@users.noreply.github.com>
2019-05-03 16:11:25 +0530

    Delete cloudConnection.js

--- cloudConnection.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://channu:channu@cluster0-zd02o.mongodb.net/test?retryWrites=true')
-.then(()=>console.log("connected to Mongdb"))
-.catch(err=>console.error("Ooops... connection error",err));
-
-
