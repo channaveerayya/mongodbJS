37ee940b70a2bd4b7d98e7af6ef387ff18cb09e1
channu <43161552+channaveerayya@users.noreply.github.com>
2019-05-03 16:11:09 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/TestDB')
-.then(()=>console.log("connected to Mongdb"))
-.catch(err=>console.error("Ooops... connection error",err));
-
-
